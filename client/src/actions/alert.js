2cd1134ba51fbed2063f5eb2b04789d928e2b9d8
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-06-08 09:05:04 -0400

    2020-Jun-8 updated all packages

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,4 +1,4 @@
-import uuid from "uuid";
+import { v4 as uuid } from "uuid";
 import { SET_ALERT, REMOVE_ALERT } from "./type";
 
 export const setAlert = (msg, alertType, timeOut = 3000) => dispatch => {
